9c1e1c65c7c03c1e5f5d9699a92bd148c07c2eda
hauzerlin <48373051+hauzerlin@users.noreply.github.com>
2021-06-20 00:18:41 +0800

    test system command

---
+++ airline/test_system.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Hello world!\n";
+    system("pause");
+    system("cls");
+    cout<<"test line"<<endl;
+
+    return 0;
+}
